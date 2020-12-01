313ff235c11636603f1a77f46827b3c1a929aeff
Bryan Gadd <45637546+Dragola@users.noreply.github.com>
2020-12-01 16:03:59 -0500

    Collectables added
    Changes:
    
    Added Collectable Items. 2 scripts where used:
    CollectableItem for detecting trigger in each object.
    CollectableItems which holds the collectableCount and recieves signal from child object that it was hit by player.
    
    Added collectable prefab (will have to change it to the actuall collectable item later on).

---
+++ Seize The Cheese/Assets/Scripts/Bryan's Scripts/CollectableItems.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableItems : MonoBehaviour
+{
+    public int collectableCount = 0;
+    public void addCollectable()
+    {
+        collectableCount += 1;
+        Debug.Log("Collectable added");
+    }
+}
