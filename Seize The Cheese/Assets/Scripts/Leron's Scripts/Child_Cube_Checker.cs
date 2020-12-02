77c0d547f24aff5e41765a894cffeaa377ede73f
Leron Bergelson <leronbergelson@gmail.com>
2020-12-02 14:46:37 -0500

    Revert "Revert "Merge branch 'main' into bryan-dev""
    This reverts commit c9224c39a0655d71ec3fc74f1cc0c0b1006bd0df.

---
+++ Seize The Cheese/Assets/Scripts/Leron's Scripts/Child_Cube_Checker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Child_Cube_Checker : MonoBehaviour
+{
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    if (transform.tag == "ChildCube" && transform.parent == null)
+    //    {
+    //        transform.tag = "CubeCheese";
+    //        transform.GetComponent<Rigidbody>().useGravity = true;
+    //        Debug.Log("wtf");
+    //    }
+    //}
+}
