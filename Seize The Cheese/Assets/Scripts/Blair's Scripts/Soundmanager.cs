ea88cc2ec3d890e025de2b6fd65b34e20dd2ec0e
Blair-White <blairjwhite@hotmail.com>
2021-03-04 01:39:07 -0500

    Added Sound Manager, fixed ui, waiting on win details

---
+++ Seize The Cheese/Assets/Scripts/Blair's Scripts/Soundmanager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soundmanager : MonoBehaviour
+{
+
+    public static Soundmanager instance = null;
+    public AudioClip JumpSound;
+    public AudioSource AudSrc;
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+        else if (instance != this) Destroy(gameObject);
+        DontDestroyOnLoad(this.gameObject);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PlaySoundOneShot(AudioClip snd, float volume)
+    {
+        AudSrc.volume = volume;
+        AudSrc.PlayOneShot(snd);
+    }
+
+}
