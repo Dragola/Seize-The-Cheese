c1745e5e97dc9b5648e487f650ac857f18d44d17
KyleTheScott <kylexscott@googlemail.com>
2021-03-26 11:22:34 -0400

    UI stuff
    Imported UI assets into game and set up Pause Menu with partial functionality

---
+++ Seize The Cheese/Assets/Scripts/Menu Scripts/ButtonShapeScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+//Script removes Transparent Button Border
+public class ButtonShapeScript : MonoBehaviour
+{
+    public float alphathreshhold = 0.1f;
+    void Start()
+    {
+        this.GetComponent<Image>().alphaHitTestMinimumThreshold = alphathreshhold;
+    }
+}
