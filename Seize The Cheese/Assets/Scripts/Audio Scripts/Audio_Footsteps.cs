13f62e8b2e51d29cfd9e300f216f94b778cb4245
DESKTOP-MJGG1E9\Ethan Schubert <ethanschubert.audio@gmail.com>
2021-04-08 09:22:44 -0400

    Added pickup footsteps

--- Seize The Cheese/Assets/Scripts/Audio Scripts/Audio_Footsteps.cs
+++ Seize The Cheese/Assets/Scripts/Audio Scripts/Audio_Footsteps.cs
@@ -11,28 +11,42 @@ public class Audio_Footsteps : MonoBehaviour
     [SerializeField]
     private AudioClip[] Backpackclips;
 
+    [SerializeField]
+    private AudioClip[] PickupStepclips;
+
     private AudioSource audioSource;
 
-    private void Awake ()
+    private void Awake()
     {
         audioSource = GetComponent<AudioSource>();
     }
 
     private void Step()
+        //This is the Step event for Mousey's standard walk animation
     {
-        AudioClip clip = GetRandomClip();
+        AudioClip clip = GetRandomStepClip();
         audioSource.pitch = Random.Range(0.5f, 1.5f);
         audioSource.PlayOneShot(clip);
 
-        AudioClip clip2 = GetRandomClip();
-        audioSource.pitch = Random.Range(0.5f, 1.5f);
-        audioSource.PlayOneShot(clip2);
     }
-    
-    private AudioClip GetRandomClip()
+
+    private AudioClip GetRandomStepClip()
     {
         return Footstepclips[UnityEngine.Random.Range(0, Footstepclips.Length)];
         return Backpackclips[UnityEngine.Random.Range(0, Backpackclips.Length)];
 
+    }
+
+    private void PickupStep()
+    //This is the PickupStep event for Mousey's walk animation while holding cheese
+    {
+        AudioClip clip = GetRandomPickStepClip();
+        audioSource.pitch = Random.Range(0.5f, 1.5f);
+        audioSource.PlayOneShot(clip);
+    }
+
+    private AudioClip GetRandomPickStepClip()
+    {
+        return PickupStepclips[UnityEngine.Random.Range(0, PickupStepclips.Length)];
     }
 }
