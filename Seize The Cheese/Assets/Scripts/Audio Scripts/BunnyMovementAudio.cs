f0205d4de0a1c5d5394a269d00b3ca5c48a79398
DESKTOP-MJGG1E9\Ethan Schubert <ethanschubert.audio@gmail.com>
2021-04-22 19:48:57 -0400

    More audio implementation

---
+++ Seize The Cheese/Assets/Scripts/Audio Scripts/BunnyMovementAudio.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BunnyMovementAudio : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip[] BunnyMoveClips;
+
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void BunnyStep()  //This is the Step event for Dust Bunnies standard walk animation
+    {
+        AudioClip clip = GetRandomStepClip();
+        audioSource.pitch = Random.Range(0.5f, 1.5f);
+        audioSource.PlayOneShot(clip);
+    }
+
+    private AudioClip GetRandomStepClip()
+    {
+        return BunnyMoveClips[UnityEngine.Random.Range(0, (BunnyMoveClips.Length) - 1)];
+    }
+}
