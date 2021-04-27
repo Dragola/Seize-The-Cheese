dfd685472fe8064a41e9f6b8b6a2aae3f36d15a9
DESKTOP-MJGG1E9\Ethan Schubert <ethanschubert.audio@gmail.com>
2021-04-27 12:16:58 -0400

    Added pipe emitters throughout the level

---
+++ Seize The Cheese/Assets/Scripts/Audio Scripts/EndLevelChime.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndLevelChime : MonoBehaviour
+{
+    public AudioClip Chime;
+    private AudioSource audioSource;
+    private bool MouseyEntered;
+
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        MouseyEntered = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag ("Player"))
+        {
+            Debug.Log("has collided");
+            audioSource.PlayOneShot(Chime);
+            MouseyEntered = true;
+        }
+    }
+}
