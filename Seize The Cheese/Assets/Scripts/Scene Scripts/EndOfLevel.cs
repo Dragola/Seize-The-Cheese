24c1fe1c9297a79b613e79e02c84927da3e626f1
Bryan Gadd <x-dragola-x@live.ca>
2021-05-04 21:32:47 -0400

    Added Credits Clip to Play at the End of the Level

--- Seize The Cheese/Assets/Scripts/Scene Scripts/EndOfLevel.cs
+++ Seize The Cheese/Assets/Scripts/Scene Scripts/EndOfLevel.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
+using UnityEditor;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 
 public class EndOfLevel : MonoBehaviour
@@ -8,12 +10,21 @@ public class EndOfLevel : MonoBehaviour
     private Canvas endOfLevelUI = null;
     public bool playerHitTrigger = false;
     private LevelStorage levelStorage = null;
+    private VideoPlayer videoPlayer = null;
+    private Canvas videoCanvas = null;
+    public bool videoStarted = false;
+    private float timer = 5;
 
-    private void Start()
+    private void Awake()
     {
+        videoCanvas = GameObject.Find("Video UI").GetComponent<Canvas>();
+        videoPlayer = GameObject.Find("Video Player").GetComponent<VideoPlayer>();
         levelStorage = GameObject.Find("LevelStorage").GetComponent<LevelStorage>();
         playerMovementScript = GameObject.Find("Mousy").GetComponent<MousyMovement>();
         endOfLevelUI = GameObject.Find("EndOfLevel UI").GetComponent<Canvas>();
+    }
+    private void Start()
+    {
         endOfLevelUI.gameObject.SetActive(false);
     }
     private void Update()
@@ -22,10 +33,20 @@ public class EndOfLevel : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.Return))
             {
-                LoopLevelScene();
+                LoadCreditVideo();
                 //SwapToLoadingScene();
                 playerHitTrigger = false;
             }
+        }
+        //subtract from timer
+        if (videoStarted && timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+        //if video is done then go to main menu
+        if (videoStarted && timer <= 0 && videoPlayer.isPlaying == false)
+        {
+            SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
         }
     }
     private void OnTriggerEnter(Collider other)
@@ -53,5 +74,22 @@ public class EndOfLevel : MonoBehaviour
     private void LoopLevelScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name, LoadSceneMode.Single);
+    }
+    private void LoadCreditVideo()
+    {
+        //prevent mousy from moving
+        GameObject.Find("Mousy").GetComponent<MousyMovement>().FreezePlayer(true);
+
+        //load clip
+        videoPlayer.clip = (VideoClip)AssetDatabase.LoadAssetAtPath("Assets/Cutscenes/Credits.mp4", typeof(VideoClip));
+
+        //play video
+        videoPlayer.Play();
+
+        videoStarted = true;
+
+        //enable canvas and video player
+        videoCanvas.gameObject.SetActive(true);
+        videoPlayer.gameObject.SetActive(true);
     }
 }
